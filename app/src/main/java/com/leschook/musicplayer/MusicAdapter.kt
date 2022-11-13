305b36f6988a575757c60101b07b67d4060077f8
Sergei Leshchenko <leshchenkoemail@gmail.com>
2022-11-13 03:56:06 +0300

    add functional for pause, play, next and previous buttons

--- app/src/main/java/com/leschook/musicplayer/MusicAdapter.kt
+++ app/src/main/java/com/leschook/musicplayer/MusicAdapter.kt
@@ -3,6 +3,8 @@ package com.leschook.musicplayer
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import android.media.MediaMetadataRetriever
 import android.util.Base64
 import android.view.LayoutInflater
@@ -11,6 +13,7 @@ import android.webkit.URLUtil.decode
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide.with
 import com.leschook.musicplayer.databinding.MusicViewBinding
 
 import kotlin.collections.ArrayList
@@ -32,12 +35,14 @@ class MusicAdapter(private val context: Context, private val musicList: ArrayLis
         holder.title.text = musicList[position].title
         holder.album.text = musicList[position].album
         holder.length.text = duration(musicList[position].length)
-        Glide.with(context)
+        with(context)
             .load(musicList[position].albumArtUri)
             .placeholder(R.mipmap.ic_launcher)
             .into(holder.image)
         holder.root.setOnClickListener {
             val intent = Intent(context, PlayerActivity::class.java)
+            intent.putExtra("index", position)
+            intent.putExtra("class", "MusicAdapter")
             ContextCompat.startActivity(context, intent, null)
         }
 
--- app/src/main/java/com/leschook/musicplayer/PlayerActivity.kt
+++ app/src/main/java/com/leschook/musicplayer/PlayerActivity.kt
@@ -1,11 +1,94 @@
 package com.leschook.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.leschook.musicplayer.databinding.ActivityPlayerBinding
+
 
 class PlayerActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var musicList : ArrayList<Music>
+        var musicPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+        var flag: Boolean = false
+    }
+
+    private lateinit var binding: ActivityPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        musicPosition = intent.getIntExtra("index", 0)
+        when (intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
+                musicList = ArrayList()
+                musicList.addAll(MainActivity.MusicList)
+                setMusic()
+                binding.playPauseBtn.setOnClickListener {
+                    if (flag) {
+                        pauseMusic()
+                    } else {
+                        playMusic()
+                    }
+                }
+                binding.previousBtn.setOnClickListener {
+                    previousOrNextMusic(false)
+                }
+                binding.nextBtn.setOnClickListener {
+                    previousOrNextMusic(true)
+                }
+            }
+        }
+    }
+
+    private fun playMusic() {
+        binding.playPauseBtn.setImageResource(R.drawable.ic_pause)
+        flag = true
+        mediaPlayer!!.start()
+    }
+
+    private fun pauseMusic() {
+        binding.playPauseBtn.setImageResource(R.drawable.ic_play)
+        flag = false
+        mediaPlayer!!.pause()
+    }
+
+    private fun previousOrNextMusic(flag: Boolean) {
+        if (flag) {
+            if (musicList.size - 1 == musicPosition) {
+                musicPosition = 0
+            } else {
+                ++musicPosition
+            }
+            setMusic()
+        } else {
+            if (musicPosition == 0) {
+                musicPosition = musicList.size - 1
+            } else {
+                --musicPosition
+            }
+            setMusic()
+        }
+    }
+
+    private fun setMusic() {
+        Glide.with(this)
+            .load(musicList[musicPosition].albumArtUri)
+            .placeholder(R.mipmap.ic_launcher)
+            .into(binding.musicImage)
+        binding.musicName.text = musicList[musicPosition].title
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer()
+        }
+        mediaPlayer!!.reset()
+        mediaPlayer!!.setDataSource(musicList[musicPosition].path)
+        mediaPlayer!!.prepare()
+        mediaPlayer!!.start()
+        flag = true
+        binding.playPauseBtn.setImageResource(R.drawable.ic_pause)
     }
 }
